90d8d8839e61689b65b93a5850a1ee1f6fb99851
binh <bnh.quangATgmail.com>
2020-06-16 11:14:46 +0700

    Implement queue.c

--- source_code/src/queue.c
+++ source_code/src/queue.c
@@ -8,12 +8,35 @@ int empty(struct queue_t * q) {
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
 	/* TODO: put a new process to queue [q] */	
+	q->proc[q->size]=proc;
+	q->size++;
+	//end TODO
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
 	/* TODO: return a pcb whose prioprity is the highest
 	 * in the queue [q] and remember to remove it from q
 	 * */
+	if(q->size!=0)
+	{
+		pcb_t *pcb_exit= q->proc[0]; int index=0;
+		for (int i=1; i< q->size;i++)
+		{
+			if(q->proc[i]->priority>q->proc[i-1]->priority)
+			{
+				pcb_exit=q->proc[i];
+				index=i;
+			}
+		}
+		for (int i=index; i<q->size;i++)
+		{
+			q->proc[i]=q->proc[i+1];
+		}
+		q->size--;
+		return pcb_exit;
+	}
+
+	//endTODO
 	return NULL;
 }
 
