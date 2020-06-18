0d137965f6227000bc5ef4362fdacfe3d03c27c3
BinhPhamQuang <Binh.pham.quang@hcmut.edu.vn>
2020-06-18 10:27:00 +0700

    edit queue.c

--- source_code/src/queue.c
+++ source_code/src/queue.c
@@ -2,41 +2,43 @@
 #include <stdlib.h>
 #include "queue.h"
 
-int empty(struct queue_t * q) {
+int empty(struct queue_t *q)
+{
 	return (q->size == 0);
 }
 
-void enqueue(struct queue_t * q, struct pcb_t * proc) {
-	/* TODO: put a new process to queue [q] */	
-	q->proc[q->size]=proc;
+void enqueue(struct queue_t *q, struct pcb_t *proc)
+{
+	/* TODO: put a new process to queue [q] */
+	q->proc[q->size] = proc;
 	q->size++;
 	//end TODO
 }
 
-struct pcb_t * dequeue(struct queue_t * q) {
+struct pcb_t *dequeue(struct queue_t *q)
+{
 	/* TODO: return a pcb whose prioprity is the highest
 	 * in the queue [q] and remember to remove it from q
 	 * */
-	if(q->size!=0)
+	if (q->size != 0)
 	{
-		pcb_t *pcb_exit= q->proc[0]; int index=0;
-		for (int i=1; i< q->size;i++)
+		struct pcb_t *pcb_exit = q->proc[0];
+		int index = 0;
+		for (int i = 1; i < q->size; i++)
 		{
-			if(q->proc[i]->priority>q->proc[i-1]->priority)
+			if (q->proc[i]->priority > pcb_exit->priority)
 			{
-				pcb_exit=q->proc[i];
-				index=i;
+				pcb_exit = q->proc[i];
+				index = i;
 			}
 		}
-		for (int i=index; i<q->size;i++)
+		for (int i = index; i < q->size; i++)
 		{
-			q->proc[i]=q->proc[i+1];
+			q->proc[i] = q->proc[i + 1];
 		}
 		q->size--;
 		return pcb_exit;
 	}
-
 	//endTODO
 	return NULL;
 }
-
